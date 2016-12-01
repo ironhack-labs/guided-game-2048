adb6cff8dbc8ef76881d6117eaadc726f509cb06
LluisArevalo <thorin119@gmail.com>
2016-12-01 16:20:07 +0100

    Add the move to right functionality

--- js/game-manager.js
+++ js/game-manager.js
@@ -35,35 +35,11 @@ GameManager.prototype._listen = function () {
   var that = this;
 
   document.addEventListener('keydown', function (event) {
-    if (event.keyCode === 38)      { moveTilesLeft(that); }
+    if (event.keyCode === 37)      { moveTilesLeft(that); }
+    else if (event.keyCode === 39) { moveTilesRight(that); }
+    // if (event.keyCode === 38)      { moveTilesLeft(that); }
     // else if(event.keyCode === 40 ) { console.log('down');  }
     // else if(event.keyCode === 37 ) { this.moveTilesLeft();  }
     // else if(event.keyCode === 39 ) { console.log('right'); }
   });
-};
-
-
-var moveTilesLeft = function (gameManager) {
-  var row0 = gameManager.board.matrix[0];
-
-  for (var i = 0; i < row0.length; i++) {
-    var newPositionY =  _furtherLeft(row0, row0[i], i);
-    gameManager.board.matrix[0][newPositionY] = gameManager.board.matrix[0][i];
-    gameManager.board.matrix[0][i] = null;
-
-    if (gameManager.board.matrix[0][newPositionY] !== null) {
-      gameManager.board.matrix[0][newPositionY].updatePosition(0,newPositionY);
-    }
-  }
-
-  gameManager._renderBoard();
-};
-
-var _furtherLeft = function (row, tile, index) {
-  for (var i = index - 1; i >= 0; i--) {
-    if (row[i] !== null)
-      return i + 1;
-  }
-
-  return 0;
 };
--- js/key-event-manager.js
+++ js/key-event-manager.js
@@ -1,14 +1,77 @@
-function KeyEventManager (gameManager) {
-  this.gameManager = gameManager;
-  console.log(this.gameManager);
-}
+var moveTilesLeft = function (gameManager) {
+  var rowSize = gameManager.board.matrix.length;
+  var colSize = gameManager.board.matrix[0].length;
+  var matrix  = gameManager.board.matrix;
 
-KeyEventManager.prototype.listen = function () {
-  console.log("Manager", this.gameManager);
-  document.body.onkeydown = function(evt) {
-    if (evt.keyCode === 38)      { console.log('up');    }
-    else if(evt.keyCode === 40 ) { console.log('down');  }
-    else if(evt.keyCode === 37 ) { this.gameManager.moveTilesLeft();  }
-    else if(evt.keyCode === 39 ) { console.log('right'); }
-  };
+  for (var i = 0; i < rowSize; i++) {
+    for (var j = 0; j < colSize; j++) {
+      var newPositionY =  _furtherLeft(matrix[i], j);
+
+      if (newPositionY === j) {
+        continue;
+      }
+
+      matrix[i][newPositionY] = matrix[i][j];
+      matrix[i][j] = null;
+
+      if (matrix[i][newPositionY] && matrix[i][newPositionY] !== null) {
+        matrix[i][newPositionY].updatePosition(i, newPositionY);
+      }
+    }
+  }
+
+  gameManager._renderBoard();
+};
+
+var moveTilesRight = function (gameManager) {
+  var rowSize = gameManager.board.matrix.length;
+  var colSize = gameManager.board.matrix[0].length;
+  var matrix  = gameManager.board.matrix;
+
+  for (var i = 0; i < rowSize; i++) {
+    for (var j = colSize - 1; j >= 0; j--) {
+      var newPositionY =  _furtherRight(matrix[i], matrix[i][j], j);
+
+      if (j === newPositionY) {
+        continue;
+      }
+
+      if (matrix[i][j] !== null) {
+        matrix[i][newPositionY] = matrix[i][j];
+        matrix[i][j] = null;
+
+        if (matrix[i][newPositionY] && matrix[i][newPositionY] !== null) {
+          matrix[i][newPositionY].updatePosition(i, newPositionY);
+        }
+      }
+    }
+  }
+
+  gameManager._renderBoard();
+};
+
+var _furtherLeft = function (row, tileCurrentY) {
+  if (tileCurrentY === 0) {
+    return 0;
+  }
+
+  for (var i = tileCurrentY - 1; i >= 0; i--) {
+    if (row[i] !== null)
+      return i + 1;
+  }
+
+  return 0;
+};
+
+var _furtherRight = function (row, tileCurrentY) {
+  if (tileCurrentY === row.length - 1) {
+    return tileCurrentY;
+  }
+
+  for (var i = tileCurrentY + 1; i < row.length - 1; i++) {
+    if (row[i] !== null)
+      return i + 1;
+  }
+
+  return row.length - 1;
 };
