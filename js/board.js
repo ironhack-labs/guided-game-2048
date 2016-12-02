3e1b27fd7e3eeff4896e019a1a771e72e9e0a894
LluisArevalo <thorin119@gmail.com>
2016-12-02 13:42:46 +0100

    Move left and right

--- js/board.js
+++ js/board.js
@@ -1,9 +1,6 @@
 function Board () {
   this.matrix = [];
-  this._initialize();
-}
 
-Board.prototype._initialize = function () {
   for (var i = 0; i < 4; i++) {
     this.matrix.push([]);
 
@@ -11,23 +8,27 @@ Board.prototype._initialize = function () {
       this.matrix[i].push(null);
     }
   }
+
+  for (i = 0; i < 2; i++) {
+    var pos = this._getAvailablePosition();
+    this.matrix[pos.x][pos.y] = this._getTileInitialValue();
+  }
+}
+
+Board.prototype._renderBoard = function () {
+  for (var i = 0; i < 4; i++) {
+    console.log(this.matrix[i]);
+  }
 };
 
-Board.prototype.getTileInitialValue = function () {
+Board.prototype._getTileInitialValue = function () {
   var initialValues = ["2", "4"];
   var random        = Math.random();
 
   return random < 0.8 ? initialValues[0] : initialValues[1];
 };
 
-Board.prototype.addTile = function (tile) {
-  var x = tile.position.x;
-  var y = tile.position.y;
-
-  this.matrix[x][y] = tile;
-};
-
-Board.prototype.getAvailablePosition = function () {
+Board.prototype._getAvailablePosition = function () {
   var availablePositions = [];
 
   for (var i = 0; i < 4; i++) {
@@ -38,14 +39,73 @@ Board.prototype.getAvailablePosition = function () {
     }
   }
 
-  if (this._gameOver(availablePositions)) {
-      return "TODO: Implement Game over";
+  if (availablePositions.length === 0) {
+    return false;
+  } else {
+    var randomPosition = Math.floor(Math.random() * availablePositions.length);
+    return availablePositions[randomPosition];
   }
-
-  var randomPosition = Math.floor(Math.random() * availablePositions.length);
-  return availablePositions[randomPosition];
 };
 
-Board.prototype._gameOver = function (availablePositions) {
-  return availablePositions.length === 0;
+Board.prototype.moveLeft = function () {
+  var newBoard = [];
+
+  this.matrix.forEach (function (row) {
+    var newRow = row.filter(function (i) { return i !== null; });
+
+    for(i = 0; i < newRow.length - 1; i++) {
+      if (newRow[i+1] === newRow[i]) {
+        newRow[i+1] = newRow[i] * 2;
+        newRow[i] = null;
+      }
+    }
+
+    var merged = newRow.filter(
+      function (i) { return i !== null;
+    });
+
+    while(merged.length < 4) {
+      merged.push(null);
+    }
+
+    newBoard.push(merged);
+  });
+
+  this.matrix = newBoard;
+  this._renderBoard();
 };
+
+Board.prototype.moveRight = function () {
+  var newBoard = [];
+
+  this.matrix.forEach (function (row) {
+    var newRow = row.filter(function (i) { return i !== null; });
+
+    for(i = newRow.length-1; i > 0; i--) {
+      if (newRow[i-1] === newRow[i]) {
+        newRow[i-1] = newRow[i] * 2;
+        newRow[i] = null;
+      }
+    }
+
+    var merged = newRow.filter(
+      function (i) { return i !== null;
+    });
+
+    while(merged.length < 4) {
+      merged.unshift(null);
+    }
+
+    newBoard.push(merged);
+  });
+
+  this.matrix = newBoard;
+  this._renderBoard();
+};
+
+var board = new Board();
+board._renderBoard();
+// board.moveDown();
+// board.moveLeft();
+// board.moveRight();
+// board.moveUp();
