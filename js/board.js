9c32d6d3d105ca0c3d048b7885d977d251c682c8
LluisArevalo <thorin119@gmail.com>
2016-12-04 20:29:40 +0100

    Change the visibility of the moveUp, moveDown, moveLeft, and moveRight functions. They don't have to be public anymore. Remove a white line. Remove the moveDown old functionality

--- js/board.js
+++ js/board.js
@@ -10,7 +10,6 @@ function Board () {
     }
   }
 
-
   for (i = 0; i < 2; i++) {
     this._generateTile();
   }
@@ -20,6 +19,7 @@ Board.prototype._renderBoard = function () {
   for (var i = 0; i < 4; i++) {
     console.log(this.matrix[i]);
   }
+
   this._printScore();
 };
 
@@ -51,7 +51,7 @@ Board.prototype._getAvailablePosition = function () {
   }
 };
 
-Board.prototype.moveLeft = function () {
+Board.prototype._moveLeft = function () {
   var newBoard = [];
   var that = this;
 
@@ -76,7 +76,7 @@ Board.prototype.moveLeft = function () {
   this.matrix = newBoard;
 };
 
-Board.prototype.moveRight = function () {
+Board.prototype._moveRight = function () {
   var newBoard = [];
   var that = this;
   this.matrix.forEach (function (row) {
@@ -100,48 +100,24 @@ Board.prototype.moveRight = function () {
   this.matrix = newBoard;
 };
 
-Board.prototype.moveUp = function () {
+Board.prototype._moveUp = function () {
   this._transposeMatrix();
-  this.moveLeft();
+  this._moveLeft();
   this._transposeMatrix();
 };
 
-Board.prototype.moveDown = function () {
-  that = this;
-  var transposed_matrix = this.matrix[0].map(function(col, i) {
-    return that.matrix.map(function(row) {
-      return row[i];
-    });
-  });
-  var newBoard = [];
-
-  transposed_matrix.forEach (function (row) {
-    var newRow = row.filter(function (i) { return i !== null; });
-
-    for(i = newRow.length-1; i > 0; i--) {
-      if (newRow[i-1] === newRow[i]) {
-        newRow[i-1] = newRow[i] * 2;
-        newRow[i] = null;
-      }
-    }
-
-    var merged = newRow.filter(function (i) { return i !== null;});
-
-    while(merged.length < 4) { merged.unshift(null); }
-
-    newBoard.push(merged);
-  });
+Board.prototype._moveDown = function () {
   this._transposeMatrix();
-  this.moveRight();
+  this._moveRight();
   this._transposeMatrix();
 };
 
 Board.prototype.move = function (direction) {
   switch (direction) {
-    case "up":    this.moveUp();    break;
-    case "down":  this.moveDown();  break;
-    case "left":  this.moveLeft();  break;
-    case "right": this.moveRight(); break;
+    case "up":    this._moveUp();    break;
+    case "down":  this._moveDown();  break;
+    case "left":  this._moveLeft();  break;
+    case "right": this._moveRight(); break;
   }
 
   this._generateTile();
@@ -173,7 +149,3 @@ Board.prototype._win = function(value) {
 
 var board = new Board();
 board._renderBoard();
-// board.moveDown();
-// board.moveLeft();
-// board.moveRight();
-// board.moveUp();
