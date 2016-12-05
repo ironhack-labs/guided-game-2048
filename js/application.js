5d42657df7c550165f2004b276505ca63a961ed0
fontcuberta <isafontcu@gmail.com>
2016-12-05 14:52:54 +0100

    Render Tiles function

--- js/application.js
+++ js/application.js
@@ -1,3 +1,22 @@
+var game;
 window.onload =function(){
-  var gameManager = new GameManager();
+  game = new GameManager();
+  renderTiles();
 };
+
+function renderTiles() {
+  game.matrix.forEach(function(row, rowIndex){
+    row.forEach(function (cell, cellIndex) {
+      if (cell !== null) {
+        var tileContainer = document.getElementById("tile-container");
+        var newTile       = document.createElement("div");
+
+        newTile.classList  = "tile val-" + cell;
+        newTile.classList += " tile-position-" + rowIndex + "-" + cellIndex;
+        newTile.innerHTML  = (cell);
+
+        tileContainer.appendChild(newTile);
+      }
+    });
+  });
+}
--- js/game-manager.js
+++ js/game-manager.js
@@ -1,62 +1,148 @@
 function GameManager () {
-  this.htmlManager = new HtmlManager();
-  this._initialize();
+  this.matrix = [];
+  this.score  = 0;
+
+  for (var i = 0; i < 4; i++) {
+    this.matrix.push([]);
+
+    for (var j = 0; j < 4; j++) {
+      this.matrix[i].push(null);
+    }
+  }
+
+  for (i = 0; i < 2; i++) {
+    this._generateTile();
+  }
 }
 
-GameManager.prototype._initialize = function () {
-  this.board = new Board();
-  this._initializeTiles();
-  this._renderBoard();
-  this._listen();
-};
-
-// GameManager.prototype._initializeTiles = function () {
-//   for (var i = 0; i < 2; i++) {
-//     var position = this.board.getAvailablePosition();
-//     var value    = this.board.getTileInitialValue();
-//     var tile     = new Tile(value, position.x, position.y);
-//
-//     this.board.addTile(tile);
-//   }
-// };
-
-GameManager.prototype._initializeTiles = function () {
-  this.board.addTile(new Tile("2", 3, 0));
-
-  this.board.addTile(new Tile("2", 3, 1));
-  this.board.addTile(new Tile("4", 2, 1));
-
-  this.board.addTile(new Tile("2", 3, 2));
-  // this.board.addTile(new Tile("128", 2, 2));
-  this.board.addTile(new Tile("8", 1, 2 ));
-
-  this.board.addTile(new Tile("2", 3, 3));
-  this.board.addTile(new Tile("4", 2, 3));
-  // this.board.addTile(new Tile("8", 1, 3));
-  this.board.addTile(new Tile("16", 0, 3));
-};
-
 GameManager.prototype._renderBoard = function () {
-  this.htmlManager.resetRendering(this.board.matrix);
+  for (var i = 0; i < 4; i++) {
+    console.log(this.matrix[i]);
+  }
 
-  for (var i = 0; i < this.board.matrix.length; i++) {
-    for (var j = 0; j < this.board.matrix[i].length; j++) {
-      var cell = this.board.matrix[i][j];
-      this.htmlManager.renderTile(cell);
+  this._printScore();
+};
+
+GameManager.prototype._generateTile = function () {
+  var initialValues = [2, 4];
+  var random        = Math.random();
+  var pos           = this._getAvailablePosition();
+
+  if (pos)
+    this.matrix[pos.x][pos.y] = random < 0.8 ? initialValues[0] : initialValues[1];
+};
+
+GameManager.prototype._getAvailablePosition = function () {
+  var availablePositions = [];
+
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+      if (this.matrix[i][j] === null) {
+        availablePositions.push({ x: i, y: j });
+      }
+    }
+  }
+
+  if (availablePositions.length === 0) {
+    return false;
+  } else {
+    var randomPosition = Math.floor(Math.random() * availablePositions.length);
+    return availablePositions[randomPosition];
+  }
+};
+
+GameManager.prototype._moveLeft = function () {
+  var newBoard = [];
+  var that = this;
+
+  this.matrix.forEach (function (row) {
+    var newRow = row.filter(function (i) { return i !== null; });
+
+    for(i = 0; i < newRow.length - 1; i++) {
+      if (newRow[i+1] === newRow[i]) {
+        var value = newRow[i] * 2;
+        newRow[i+1] = value;
+        that._updateScore(value);
+        console.log(that._win(value));
+        newRow[i] = null;
+      }
+    }
+
+    var merged = newRow.filter( function (i) { return i !== null; });
+    while(merged.length < 4) { merged.push(null); }
+    newBoard.push(merged);
+  });
+
+  this.matrix = newBoard;
+};
+
+GameManager.prototype._moveRight = function () {
+  var newBoard = [];
+  var that = this;
+  this.matrix.forEach (function (row) {
+    var newRow = row.filter(function (i) { return i !== null; });
+
+    for(i = newRow.length-1; i > 0; i--) {
+      if (newRow[i-1] === newRow[i]) {
+        var value = newRow[i] * 2;
+        newRow[i-1] = value;
+        that._updateScore(value);
+        console.log(that._win(value));
+        newRow[i] = null;
+      }
+    }
+
+    var merged = newRow.filter( function (i) { return i !== null; });
+    while(merged.length < 4) { merged.unshift(null); }
+    newBoard.push(merged);
+  });
+
+  this.matrix = newBoard;
+};
+
+GameManager.prototype._moveUp = function () {
+  this._transposeMatrix();
+  this._moveLeft();
+  this._transposeMatrix();
+};
+
+GameManager.prototype._moveDown = function () {
+  this._transposeMatrix();
+  this._moveRight();
+  this._transposeMatrix();
+};
+
+GameManager.prototype.move = function (direction) {
+  switch (direction) {
+    case "up":    this._moveUp();    break;
+    case "down":  this._moveDown();  break;
+    case "left":  this._moveLeft();  break;
+    case "right": this._moveRight(); break;
+  }
+
+  this._generateTile();
+  this._renderBoard();
+};
+
+GameManager.prototype._transposeMatrix = function() {
+  for (var row = 0; row < this.matrix.length; row++) {
+    for (var column = row+1; column < this.matrix.length; column++) {
+      var temp = this.matrix[row][column];
+      this.matrix[row][column] = this.matrix[column][row];
+      this.matrix[column][row] = temp;
     }
   }
 };
 
-GameManager.prototype._listen = function () {
-  var that = this;
+GameManager.prototype._updateScore = function(pointsGained) {
+  this.score += pointsGained;
+};
 
-  document.addEventListener('keydown', function (event) {
-    if (event.keyCode === 37)      { moveTilesLeft(that); }
-    else if (event.keyCode === 39) { moveTilesRight(that); }
-    else if (event.keyCode === 38) { moveTilesUp(that); }
-    else if (event.keyCode === 40) { moveTilesDown(that); }
+GameManager.prototype._printScore = function() {
+  console.log("Score:", this.score);
+};
 
-    that._renderBoard();
-  });
-
+GameManager.prototype._win = function(value) {
+  if (value == 2048)
+    return true;
 };
